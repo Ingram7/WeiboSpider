04bd5592be6435059646c20e14130548c2b81296
Ingram7 <48173872+Ingram7@users.noreply.github.com>
2019-08-15 20:24:15 +0800

    Update middlewares.py

--- WeiboSpider/middlewares.py
+++ WeiboSpider/middlewares.py
@@ -23,8 +23,8 @@ import base64
 # 代理服务器
 proxyServer = "http://http-dyn.abuyun.com:9020"
 # 代理隧道验证信息
-proxyUser = "HF2N901MA0B18K9D"
-proxyPass = "18A4C9B964211B00"
+proxyUser = "  "
+proxyPass = "  "
 proxyAuth = "Basic " + base64.urlsafe_b64encode(bytes((proxyUser + ":" + proxyPass), "ascii")).decode("utf8")
 class ProxyMiddleware(object):
     def process_request(self, request, spider):
